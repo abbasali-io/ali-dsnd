178d8919100f9c3271509bddc679eaa8f62a163f
Abbas <hello@abbasali.io>
2022-03-22 23:53:28 +0800

    prrocess_data is creating database file - update project 2 readme - process_data.py is creating the database successfully

--- 2- Disaster Response Pipeline/README.md
+++ 2- Disaster Response Pipeline/README.md
@@ -1,14 +1,37 @@
 # Disaster Response Pipeline Project
 
-### Instructions:
+## Project Description
+
+In this Project, I created an ETL pipeline for data transformation and a Machine Learning pipeline to classify the messages into multiple classes. The pipeline build a model which is used in a Flask application. The application is deployed at servers to be used by the disaster management staff, by putting in a tweeted message and getting categories that can be related to the message. Moving forward the model can be part of an automated pipeline to detect the required categories of tweets automatically and respond to the emergency workers as required.
+
+## Project Assets
+
+The project uses following key assets;
+
+`./notebooks/ETL Pipeline Preperation.ipynb` <- _The Data Experiments Notebook_
+
+`./notebooks/Disaster_Response_ML_Pipeline_Preperation.ipynb` <- _The Machine Learning Model Experiments Notebook_
+
+`./data/process_data.py` <- _Use this file to generate database file_
+
+`./model/train_classifier.py` <- _Use this file to generate machine learning model_
+
+`./app/templates/\*.html`<- _Use this folder files to manipulate the html_
+
+`./app/run.py` <- _Use this file to run the program_
+
+## Instructions:
+
 1. Run the following commands in the project's root directory to set up your database and model.
 
-    - To run ETL pipeline that cleans data and stores in database
-        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
-    - To run ML pipeline that trains classifier and saves
-        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+   - **To run ETL pipeline that cleans data and stores in database**
+     `cd 2- Disaster Response Pipeline`
+     `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+
+   - To run ML pipeline that trains classifier and saves
+     `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
 
 2. Run the following command in the app's directory to run your web app.
-    `python run.py`
+   `python run.py`
 
-3. Go to http://0.0.0.0:3001/
+3. Go to http://0.0.0.0:5000/
--- 2- Disaster Response Pipeline/data/process_data.py
+++ 2- Disaster Response Pipeline/data/process_data.py
@@ -1,16 +1,98 @@
+# Run the file by the following
+# cd 2- Disaster Response Pipeline
+# python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db
+
+from dataclasses import replace
 import sys
+import pandas as pd
+import numpy as np
+import sqlalchemy as db
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Description:
+        Load Data from the csv files
+
+    Args:
+        messages_filepath: The filepath of the csv file containing actual tweet messages
+        categories_filepath: The filepath of the csv file containing message categories
+
+    Return:
+        df: dataframe containing messages and categoreis
+    """
+
+    # Load messages and categories
+    messages = pd.read_csv('data/disaster_messages.csv')
+    categories = pd.read_csv('data/disaster_categories.csv')
+    df = pd.merge(messages, categories, on="id")
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Description:
+        Cleans the data in the raw merged dataframe of messages and categories into
+        a processable dataframe ready to export
+
+        Args:
+            df: The raw dataframe containign messages and categories
+
+        Return:
+            df: The cleaned dataframe ready to be loaded to a database    
+    """
+
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(";", n=-1, expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # now lets get rid of last two characters by slicing the
+    # string using lambda function
+    category_colnames = row.apply(lambda x: x[:-2])
+
+    # rename the 'categories' column'
+    categories.columns = category_colnames
+
+    # Convert category values to just numbers 0 or 1
+    for column in categories.columns:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int')
+
+    # drop the original 'categories' column from our dataframe `df`
+    if 'categories' in df.columns:
+        df.drop('categories', axis='columns', inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    # return the dataframe
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Description:
+        Load the provided dataframe into a new sqlite database with the name provided 
+        into the table named 'messages'
+
+    Args:
+        df: The dataframe containing data to be loaded
+        database_filename: The name of the database file
+
+    Return:
+        Void
+    """
+
+    # Create the database file for next steps and export
+    engine = db.create_engine('sqlite:///'+database_filename+'.db')
+    df.to_sql('messages', engine, if_exists='replace', index=False)
 
 
 def main():
@@ -24,18 +106,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
